604ce0806cd5145433dc78de0dfb891ff4b9dd83
rixhabh-k <kharwarrishabh891@gmail.com>
2026-07-10 16:26:02 +0530

    minor chnages

---
+++ client/src/components/Coming Soon/ComingSoon.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./Module.css"
+
+const ComingSoon = () => {
+  return (
+     <section className="coming-section">
+
+      <div className="coming-container">
+
+        <div className="coming-header">
+          <h2>NEW RELEASES</h2>
+
+          <div className="coming-divider"></div>
+
+          <p>EXPLORE THE EXCLUSIVE DEBUT DROPS FROM KAMIGAMI</p>
+        </div>
+
+        <div className="coming-content">
+
+          <h1 className="coming-title">
+            <span>COMING</span>
+            <span>SOON</span>
+          </h1>
+
+        </div>
+
+      </div>
+
+    </section>
+  )
+}
+
+export default ComingSoon
